c9fe8e02260a8fa464af547782ad45c3f8740f98
Philip Helger <philip@helger.com>
2016-05-27 18:45:13 +0200

    Added JavaDoc

--- ph-datetime/src/main/java/com/helger/datetime/domain/EDayOfWeek.java
+++ ph-datetime/src/main/java/com/helger/datetime/domain/EDayOfWeek.java
@@ -58,7 +58,7 @@ public enum EDayOfWeek implements IHasIntID
   }
 
   /**
-   * @return The JDK8 time ID
+   * @return The JDK8 time ID (1-based!)
    */
   public int getDateTimeConstant ()
   {
@@ -66,7 +66,7 @@ public enum EDayOfWeek implements IHasIntID
   }
 
   /**
-   * @return The java.util.Calendar ID
+   * @return The java.util.Calendar ID (1-based!)
    */
   public int getCalendarConstant ()
   {
